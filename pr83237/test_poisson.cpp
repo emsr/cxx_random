70f18f0c2195f0148742f7293b3f6d24178203ba
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-12-18 11:22:41 -0500

    Make test_poisson.cpp use a histogram instead of writing to a file.

--- pr83237/test_poisson.cpp
+++ pr83237/test_poisson.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <iostream>
 #include <random>
+#include "../histogram.h"
 
 int
 main()
@@ -13,9 +14,14 @@ main()
   // The problem turned out to be independent on seed
   engine.seed(1);
   std::poisson_distribution<int> distribution(157.17);
+  histogram<int> hist(110, 200, 90);
 
-  for (int i = 0; i < 1E8; i++) {
+  for (int i = 0; i < 1E8; i++)
+  {
     const int number = distribution(engine);
-    std::cout << number << '\n';
+    //std::cout << number << '\n';
+    hist << number;
   }
+  for (auto& bin : hist)
+    std::cout << bin << '\n';
 }
