2a3a95b5cbd0c4c07619335bdbf362b51006ab15
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-12-22 14:08:59 -0500

    This version of the uniform in tetrahedron distro works very well and quickly.

--- Makefile
+++ Makefile
@@ -12,3 +12,8 @@ test_uniform_inside_triangle_distribution: test_uniform_inside_triangle_distribu
 
 test_uniform_inside_tetrahedron_distribution: test_uniform_inside_tetrahedron_distribution.cpp
 	$(HOME)/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -o test_uniform_inside_tetrahedron_distribution test_uniform_inside_tetrahedron_distribution.cpp
+
+test:
+	./test_uniform_inside_sphere_distribution > test_uniform_inside_sphere_distribution.txt
+	./test_uniform_inside_triangle_distribution > test_uniform_inside_triangle_distribution.txt
+	./test_uniform_inside_tetrahedron_distribution > test_uniform_inside_tetrahedron_distribution.txt
--- test_uniform_inside_tetrahedron_distribution.cpp
+++ test_uniform_inside_tetrahedron_distribution.cpp
@@ -8,15 +8,111 @@ $HOME/bin/bin/g++ -std=gnu++11 -g -Wall -Wextra -o test_uniform_inside_tetrahedr
 #include <iomanip>
 #include <fstream>
 
-// This seems to fail.
-// I can't figure out a reflection formula.
-// Rejection will be expensive (1/6 acceptance).
-template<class RealType = double>
+template<class RealTp = double>
+  constexpr std::array<std::array<RealTp, 3>, 4>
+  tetra0{{{1, 1, 1}, {0, 0, 1}, {1, 0, 0}, {0, 1, 0}}};
+
+template<class RealTp = double>
+  constexpr std::array<std::array<RealTp, 3>, 4>
+  tetra1{{{0, 0, 0}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}}};
+
+template<class RealTp = double>
+  constexpr std::array<std::array<RealTp, 3>, 4>
+  tetra2{{{1, 0, 1}, {0, 0, 1}, {1, 0, 0}, {1, 1, 1}}};
+
+template<class RealTp = double>
+  constexpr std::array<std::array<RealTp, 3>, 4>
+  tetra3{{{0, 1, 1}, {0, 1, 0}, {0, 0, 1}, {1, 1, 1}}};
+
+template<class RealTp = double>
+  constexpr std::array<std::array<RealTp, 3>, 4>
+  tetra4{{{1, 1, 0}, {0, 1, 0}, {1, 0, 0}, {1, 1, 1}}};
+
+template<class RealTp = double>
+  RealTp
+  volume(const std::array<std::array<RealTp, 3>, 4>& tetra)
+  {
+    std::array<RealTp, 3> u, v, w;
+    for (auto c = 0; c < 3; ++c)
+      {
+	u[c] = tetra[1][c] - tetra[0][c];
+	v[c] = tetra[2][c] - tetra[0][c];
+	w[c] = tetra[3][c] - tetra[0][c];
+      }
+    return w[0] * (u[1] * v[2] - u[2] * v[1])
+	 + w[1] * (u[2] * v[0] - u[0] * v[2])
+	 + w[2] * (u[0] * v[1] - u[1] * v[0]);
+  }
+
+template<class RealTp = double>
+  std::array<RealTp, 4>
+  barycenter(const std::array<std::array<RealTp, 3>, 4>& tetra,
+	     const std::array<RealTp, 3>& point)
+  {
+    auto vol = volume(tetra);
+    std::array<RealTp, 4> bary;
+    for (auto v = 0; v < 4; ++v)
+      {
+	auto tet = tetra;
+	for (auto c = 0; c < 3; ++c)
+	  tet[v][c] = point[c];
+	bary[v] = volume(tet) / vol;
+      }
+
+    return bary;
+  }
+
+template<class RealTp = double>
+  bool
+  valid(const std::array<RealTp, 4>& bary)
+  {
+    const auto eps = RealTp{10} * std::numeric_limits<RealTp>::epsilon();
+    auto sum = RealTp{0};
+    for (int v = 0; v < 4; ++v)
+      {
+	if (bary[v] < RealTp{0} || bary[v] > RealTp{1})
+	  return false;
+        sum += bary[v];
+      }
+
+    return std::abs(sum - RealTp{1}) < eps;
+  }
+
+/**
+ * This class generates points uniformly over a tetrahedron in the form of
+ * four-component barycentric coordinates useable for arbitrary tetrahedra.
+ * This generator works by obtaining uniformly distributed points
+ * over the unit cube.
+ * Then the points are identified as being inside one of five cube-filling
+ * tetrahedra*:
+ * @f[
+ *   T_0 = ((1, 1, 1), (0, 0, 1), (1, 0, 0), (0, 1, 0))
+ *   T_1 = ((0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1))
+ *   T_2 = ((1, 0, 1), (0, 0, 1), (1, 0, 0), (1, 1, 1))
+ *   T_3 = ((0, 1, 1), (0, 1, 0), (0, 0, 1), (1, 1, 1))
+ *   T_4 = ((1, 1, 0), (0, 1, 0), (1, 0, 0), (1, 1, 1))
+ * @f]
+ * Finally, each point is converted to the barycentric coordinates of its
+ * containing tetrahedron and returned.
+ *
+ * This distribution uses the uniform real distribution over its default range
+ * of @f$ [0, 1) @f$ and is thus stateless.
+ *
+ * This technique is a generalization of the procedure used in the uniform
+ * inside triangle distribution.  Adapting this to higher dimensions is
+ * expected to be very difficult and of diminishing utility.
+ *
+ * *This tetrahedralization can be constructed by clipping every other vertex
+ * of the cube through the adjacent vertices.  This produces four tetrahedral
+ * pyramids of base edge length @f$ \sqrt{2} @f$ and sloping edge length 1
+ * and leaves behind a regular tetrahedron of edge length @f$ \sqrt{2} @f$.
+ */
+template<class RealTp = double>
   class uniform_inside_tetrahedron_distribution
   {
   public:
 
-    using result_type = std::array<RealType, 4>;
+    using result_type = std::array<RealTp, 4>;
     struct param_type
     { };
 
@@ -44,37 +140,25 @@ template<class RealType = double>
       result_type
       operator()(Generator& __gen)
       {
-	//  Pick three random barycentric coordinates.
-	result_type __bary;
-	__bary[1] = this->_M_urd(__gen);
-	__bary[2] = this->_M_urd(__gen);
-	__bary[3] = this->_M_urd(__gen);
-	if (__bary[1] + __bary[2] + __bary[3] > RealType{2})
-	  {
-            __bary[1] = RealType{1} - __bary[1];
-            __bary[2] = RealType{1} - __bary[2];
-            __bary[3] = RealType{1} - __bary[3];
-	  }
-	else if (__bary[2] + __bary[3] > RealType{1})
-	  {
-            __bary[2] = RealType{1} - __bary[2];
-            __bary[3] = RealType{1} - __bary[3];
-	  }
-	else if (__bary[3] + __bary[1] > RealType{1})
-	  {
-            __bary[3] = RealType{1} - __bary[3];
-            __bary[1] = RealType{1} - __bary[1];
-	  }
-	else if (__bary[1] + __bary[2] > RealType{1})
-	  {
-            __bary[1] = RealType{1} - __bary[1];
-            __bary[2] = RealType{1} - __bary[2];
-	  }
-	__bary[0] = RealType{1} - __bary[1] - __bary[2] - __bary[3];
-	//if (__bary[0] < RealType{0})
-	//  __bary[0] = RealType{1} + __bary[0];
+	while (true)
+	{
+	  //  Pick three random barycentric coordinates.
+	  std::array<RealTp, 3> __point;
+	  __point[0] = this->_M_urd(__gen);
+	  __point[1] = this->_M_urd(__gen);
+	  __point[2] = this->_M_urd(__gen);
 
-        return __bary;
+          if (auto bary = barycenter(tetra0<RealTp>, __point); valid(bary))
+	    return bary;
+          if (auto bary = barycenter(tetra1<RealTp>, __point); valid(bary))
+	    return bary;
+          if (auto bary = barycenter(tetra2<RealTp>, __point); valid(bary))
+	    return bary;
+          if (auto bary = barycenter(tetra3<RealTp>, __point); valid(bary))
+	    return bary;
+          if (auto bary = barycenter(tetra4<RealTp>, __point); valid(bary))
+	    return bary;
+	}
       }
 
     template<typename Generator>
@@ -84,67 +168,82 @@ template<class RealType = double>
 
     constexpr result_type
     min() const
-    { return std::array<RealType, 3>{0, 0, 0, 0}; }
+    { return std::array<RealTp, 3>{0, 0, 0, 0}; }
 
     constexpr result_type
     max() const
-    { return std::array<RealType, 3>{1, 1, 1, 1}; }
+    { return std::array<RealTp, 3>{1, 1, 1, 1}; }
 
   private:
 
-    std::uniform_real_distribution<RealType> _M_urd;
+    std::uniform_real_distribution<RealTp> _M_urd;
   };
 
 // This thing is really stateless.
-template<typename RealType>
-  bool
-  operator==(const uniform_inside_tetrahedron_distribution<RealType>&,
-	     const uniform_inside_tetrahedron_distribution<RealType>&)
+template<typename RealTp>
+  constexpr bool
+  operator==(const uniform_inside_tetrahedron_distribution<RealTp>&,
+	     const uniform_inside_tetrahedron_distribution<RealTp>&)
   { return true; }
 
-template<typename RealType>
-  bool
-  operator!=(const uniform_inside_tetrahedron_distribution<RealType>&,
-	     const uniform_inside_tetrahedron_distribution<RealType>&)
+template<typename RealTp>
+  constexpr bool
+  operator!=(const uniform_inside_tetrahedron_distribution<RealTp>&,
+	     const uniform_inside_tetrahedron_distribution<RealTp>&)
   { return false; }
 
-template<typename CharT, typename Traits, typename RealType>
+template<typename CharT, typename Traits, typename RealTp>
   std::basic_ostream<CharT,Traits>&
   operator<<(std::basic_ostream<CharT, Traits>& __os,
-	     const uniform_inside_tetrahedron_distribution<RealType>&)
+	     const uniform_inside_tetrahedron_distribution<RealTp>&)
   { return __os; }
 
-template<typename CharT, typename Traits, typename RealType>
+template<typename CharT, typename Traits, typename RealTp>
   std::basic_istream<CharT,Traits>&
   operator>>(std::basic_istream<CharT, Traits>& __is,
-	     const uniform_inside_tetrahedron_distribution<RealType>&)
+	     const uniform_inside_tetrahedron_distribution<RealTp>&)
   { return __is; }
 
 
 
 /* Test Code */
 
-template<typename CharT, typename Traits, typename RealType>
+template<typename CharT, typename Traits, typename RealTp>
   std::basic_ostream<CharT,Traits>&
   operator<<(std::basic_ostream<CharT, Traits>& __os,
-	     const std::array<RealType, 3>& __arr)
+	     const std::array<RealTp, 3>& __arr)
   { return __os << ' ' << __arr[0] << ' ' << __arr[1] << ' ' << __arr[2]; }
 
-template<typename CharT, typename Traits, typename RealType>
+template<typename CharT, typename Traits, typename RealTp>
   std::basic_ostream<CharT,Traits>&
   operator<<(std::basic_ostream<CharT, Traits>& __os,
-	     const std::array<RealType, 4>& __arr)
+	     const std::array<RealTp, 4>& __arr)
   { return __os << ' ' << __arr[0] << ' ' << __arr[1] << ' ' << __arr[2] << ' ' << __arr[3]; }
 
+/**
+ * A little tetrahedron class.
+ */
 template<typename RealTp>
   struct tetra
   {
     std::array<std::array<RealTp, 3>, 4> vert;
 
+    /**
+     * Default constructor.
+     * Construct a tetrahedron with one vertex at the origin
+     * and the other three at x = 1, y = 1, and z = 1 respectively.
+     */
     tetra()
     : vert{{{0,0,0},{1,0,0},{0,1,0},{0,0,1}}}
     { }
 
+    /**
+     * Constructor taking four vertices.
+     * Imagining A to be 'above' the triangle formed by BCD the vertices
+     * BCD should traverse the triangle in a 'clockwise' direction to form
+     * a positive-volume tetrahedron so that in the usual right hand rule
+     * convention all the triangle normals face outward from the tetrahedron.
+     */
     tetra(const std::array<RealTp, 3>& A,
 	  const std::array<RealTp, 3>& B,
 	  const std::array<RealTp, 3>& C,
@@ -152,6 +251,9 @@ template<typename RealTp>
     : vert{A, B, C, D}
     { }
 
+    /**
+     * Given a point in barycentric coordinates return the point in R3.
+     */
     std::array<RealTp, 3>
     operator()(const std::array<RealTp, 4>& bary) const
     {
@@ -159,6 +261,43 @@ template<typename RealTp>
 	bary[0] * vert[0][0] + bary[1] * vert[1][0] + bary[2] * vert[2][0] + bary[3] * vert[3][0],
 	bary[0] * vert[0][1] + bary[1] * vert[1][1] + bary[2] * vert[2][1] + bary[3] * vert[3][1],
 	bary[0] * vert[0][2] + bary[1] * vert[1][2] + bary[2] * vert[2][2] + bary[3] * vert[3][2]};
+    }
+
+    /**
+     * Given a point in R3 return the point in barycentric coordinates.
+     */
+    std::array<RealTp, 4>
+    operator()(const std::array<RealTp, 3>& point)
+    {
+      auto vol = volume(vert);
+      std::array<RealTp, 4> bary;
+      for (auto v = 0; v < 4; ++v)
+	{
+	  auto tet = vert;
+	  for (auto c = 0; c < 3; ++c)
+	    tet[v][c] = point[c];
+	  bary[v] = volume(tet) / vol;
+	}
+
+      return bary;
+    }
+
+    /**
+     * Return the volume of the tetrahedron.
+     */
+    RealTp
+    volume() const
+    {
+      std::array<RealTp, 3> u, v, w;
+      for (auto c = 0; c < 3; ++c)
+	{
+	  u[c] = vert[1][c] - vert[0][c];
+	  v[c] = vert[2][c] - vert[0][c];
+	  w[c] = vert[3][c] - vert[0][c];
+	}
+      return w[0] * (u[1] * v[2] - u[2] * v[1])
+	   + w[1] * (u[2] * v[0] - u[0] * v[2])
+	   + w[2] * (u[0] * v[1] - u[1] * v[0]);
     }
   };
 
